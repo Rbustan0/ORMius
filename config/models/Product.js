7ea66dc5614224f92885a17a2ef9cdcb1fccf7dc
Rbustan0 <59246279+Rbustan0@users.noreply.github.com>
2023-04-14 23:39:47 -0700

    Completed models folder

--- config/models/Product.js
+++ config/models/Product.js
@@ -9,7 +9,40 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2), //precision 10 digits on scale of 2 digits
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+     type: DataTypes.INTEGER,
+     references: {
+      model: 'category',
+      key: 'id',
+      // adding this might take out later
+      onDelete: 'CASCADE'
+     }
+    }
   },
   {
     sequelize,
