6aa31db14c08687a35877412d5044242cee11171
Rbustan0 <59246279+Rbustan0@users.noreply.github.com>
2023-04-17 19:56:11 -0700

    get routes done

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -24,9 +24,27 @@ catch(err){
 
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try{
+    const tag = await Tag.findByPk(req.params.id, {
+      include:[{
+        model: Product,
+        through: ProductTag
+      }]
+    });
+
+    if(!tag){
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+
+    res.status(200).json(tag);
+  }
+  catch(err){
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
