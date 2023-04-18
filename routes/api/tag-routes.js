5db10dc7b04bcf46a9a54bc4aefc4297c2df2b7a
Rbustan0 <59246279+Rbustan0@users.noreply.github.com>
2023-04-17 22:33:12 -0700

    all routes done

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -47,16 +47,54 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try{
+    const tag = await Tag.create(req.body);
+    res.status(200).json(tag);
+  }
+  catch(err){
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try{
+    const tag = await Tag.findByPk(req.params.id);
+    
+    if (!tag){
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+
+    await tag.update(req.body);
+    res.status(200).json(tag);
+
+    
+  }
+  catch(err){
+    res.status(500).json(err);
+  }
+
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try{
+    const tag = await Tag.findByPk(req.params.id);
+
+    if (!tag){
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+
+    await tag.destroy();
+    res.status(200).json({ message: 'Tag deleted!' });
+  }
+  catch(err){
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
